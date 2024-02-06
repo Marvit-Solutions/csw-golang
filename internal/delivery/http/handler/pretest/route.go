b510cdac1589c29d179d1d264c14b2e0d4875c16
berrylradianh <berrylhamesha@gmail.com>
2024-02-06 20:28:22 +0700

    fix pretest routing group

--- internal/delivery/http/handler/pretest/route.go
+++ internal/delivery/http/handler/pretest/route.go
@@ -3,7 +3,7 @@ package pretest
 import "github.com/gin-gonic/gin"
 
 func (pretestHandler *PretestHandler) RegisterRoutes(r *gin.RouterGroup) {
-	testimonialGroup := r.Group("/pretest")
-	testimonialGroup.GET("/all", pretestHandler.GetAllPretests)
-	testimonialGroup.GET("/:id/soal", pretestHandler.GetPretestById)
+	pretestGroup := r.Group("/pretest")
+	pretestGroup.GET("/all", pretestHandler.GetAllPretests)
+	pretestGroup.GET("/:id/soal", pretestHandler.GetPretestById)
 }
