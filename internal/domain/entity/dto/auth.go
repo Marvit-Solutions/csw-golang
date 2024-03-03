8b1dc5edbc8bdcda00592478c5b33788debecc44
berrylradianh <berrylhamesha@gmail.com>
2024-03-04 00:45:05 +0700

    fix: auth response

--- internal/domain/entity/dto/auth.go
+++ internal/domain/entity/dto/auth.go
@@ -2,11 +2,11 @@ package dto
 
 type AuthResponse struct {
 	ID             string `json:"ID" form:"ID"`
-	GoogleID       string `json:"GoogleID" form:"GoogleID"`
-	FacebookID     string `json:"FacebookId" form:"FacebookId"`
-	Email          string `json:"Email" form:"Email" validate:"required,email"`
+	GoogleID       string `json:"GoogleID"`
+	FacebookID     string `json:"FacebookID"`
+	Email          string `json:"Email"`
 	Password       string `json:"-"`
-	Name           string `json:"Name" form:"Name"`
-	Token          string `json:"Token" form:"Token"`
-	ProfilePicture string `json:"FotoProfil" form:"FotoProfil" default:"assets/img/users/profile/account.png"`
+	Name           string `json:"Name"`
+	Token          string `json:"Token"`
+	ProfilePicture string `json:"ProfilePicture"`
 }
