3a6775472a498a7379802f4cb9c89b1a356525b4
berrylradianh <berrylhamesha@gmail.com>
2024-03-13 15:45:48 +0700

    fix: migration script

--- script/migrate_down.sh
+++ script/migrate_down.sh
@@ -1,23 +1,25 @@
 #!/bin/zsh
 
 # Parse command-line arguments
-while getopts ":p:d:" opt; do
-case $opt in
-p) password="$OPTARG"
-;;
-d) database="$OPTARG"
-;;
-\?) echo "Invalid option: -$OPTARG" >&2
-exit 1
-;;
+while getopts ":u:p:d:" opt; do
+        case $opt in
+                u) user="$OPTARG"
+                ;;
+                p) password="$OPTARG"
+                ;;
+                d) database="$OPTARG"
+                ;;
+                \?) echo "Invalid option: -$OPTARG" >&2
+        exit 1
+        ;;
 esac
 done
 
-# Check if both password and database values are provided
-if [ -z "$password" ] || [ -z "$database" ]; then
-echo "Error: Please provide values for password (-p) and database (-d)." >&2
-exit 1
+# Check if user, password, and database values are provided
+if [ -z "$user" ] || [ -z "$password" ] || [ -z "$database" ]; then
+        echo "Error: Please provide values for user (-u), password (-p), and database (-d)." >&2
+        exit 1
 fi
 
-# Perform migration with the provided password and database
-migrate -source file://library/migration -database "postgres://postgres:$password@localhost:5432/$database?sslmode=disable" down
+# Perform migration with the provided user, password, and database
+migrate -source file://library/migration -database "postgres://$user:$password@localhost:5432/$database?sslmode=disable" down
