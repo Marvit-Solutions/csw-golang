4de41ed4a1f67761e34eccc41deb2b37f5e994a5
berrylradianh <berrylhamesha@gmail.com>
2024-03-13 18:56:32 +0700

    fix: insert role

--- library/migration/20240313095604_insert_roles.up.sql
+++ library/migration/20240313095604_insert_roles.up.sql
@@ -1,4 +1,5 @@
 INSERT INTO public.roles (id, name)
 VALUES 
 ('5448454b-5ab0-4674-af53-c74f07cee893', 'admin'),
-('ca13b18f-9f05-4db6-969a-cbdfe0c5627f', 'user');
+('ca13b18f-9f05-4db6-969a-cbdfe0c5627f', 'user'),
+('da6102a2-f39a-4fdb-bdaf-a63d7102bda4', 'mentor');
