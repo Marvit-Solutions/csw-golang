6ac8fbe084a756a94f747fa4722f5a51e63799d5
berrylradianh <berrylhamesha@gmail.com>
2024-04-10 18:37:15 +0700

    feat: change migration for quiz

--- library/migration/20240406185019_create_table.up.sql
+++ library/migration/20240406185019_create_table.up.sql
@@ -195,7 +195,7 @@ CREATE TABLE IF NOT EXISTS public.quiz_submissions (
 CREATE TABLE IF NOT EXISTS public.quizzes (
         id SERIAL PRIMARY KEY,
         uuid UUID NOT NULL DEFAULT uuid_generate_v4(),
-        sub_subject_id integer NOT NULL,
+        subject_id integer NOT NULL,
         test_type_id integer NOT NULL,
         open timestamp with time zone NOT NULL,
         title character varying(50) NOT NULL,
