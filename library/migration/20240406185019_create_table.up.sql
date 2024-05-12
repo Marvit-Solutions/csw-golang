3c82a02c24aef15bbb6814912abaa8dda82e1b3f
berrylradianh <berrylhamesha@gmail.com>
2024-05-12 19:58:55 +0700

    fix: delete created_by and updated_by on class_users

--- library/migration/20240406185019_create_table.up.sql
+++ library/migration/20240406185019_create_table.up.sql
@@ -19,8 +19,6 @@ CREATE TABLE IF NOT EXISTS public.class_users (
         uuid uuid NOT NULL DEFAULT uuid_generate_v4(),
         name character varying(100) NOT NULL,
         slug character varying(50) NOT NULL,
-        created_by integer NOT NULL,
-        updated_by integer NOT NULL,
         created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
         updated_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
         deleted_at timestamp with time zone,
