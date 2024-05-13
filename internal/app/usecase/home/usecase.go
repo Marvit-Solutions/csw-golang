ac4383046a47b8774a004f0d1faeb83061bf203b
berrylradianh <berrylhamesha@gmail.com>
2024-05-13 23:21:33 +0700

    style: delete unused repository and service

--- internal/app/usecase/home/usecase.go
+++ internal/app/usecase/home/usecase.go
@@ -20,37 +20,19 @@ type Usecase interface {
 }
 
 type usecase struct {
-	db                        *gorm.DB
-	localHomeRepo             localrepository.Home
-	userRepo                  repository.UserRepository
-	userDetailRepo            repository.UserDetailRepository
-	userMentorTestimonialRepo repository.UserMentorTestimonialRepository
-	userTestimonialRepo       repository.UserTestimonialRepository
-	classUserRepo             repository.ClassUserRepository
-	roleRepo                  repository.RoleRepository
-	mentorRepo                repository.MentorRepository
-	moduleRepo                repository.ModuleRepository
-	uniqueRepo                repository.UniqueRepository
-	planRepo                  repository.PlanRepository
-	mediaRepo                 repository.MediaRepository
+	db            *gorm.DB
+	localHomeRepo localrepository.Home
+	uniqueRepo    repository.UniqueRepository
+	mediaRepo     repository.MediaRepository
 }
 
 func NewUsecase(
 	db *gorm.DB,
 ) Usecase {
 	return &usecase{
-		db:                        db,
-		localHomeRepo:             localservice.NewHomeService(db),
-		userRepo:                  service.NewUserService(db, nil),
-		userDetailRepo:            service.NewUserDetailService(db, nil),
-		userMentorTestimonialRepo: service.NewUserMentorTestimonialService(db, nil),
-		userTestimonialRepo:       service.NewUserTestimonialService(db, nil),
-		classUserRepo:             service.NewClassUserService(db, nil),
-		roleRepo:                  service.NewRoleService(db, nil),
-		mentorRepo:                service.NewMentorService(db, nil),
-		moduleRepo:                service.NewModuleService(db, nil),
-		uniqueRepo:                service.NewUniqueService(db, nil),
-		planRepo:                  service.NewPlanService(db, nil),
-		mediaRepo:                 service.NewMediaService(db, nil),
+		db:            db,
+		localHomeRepo: localservice.NewHomeService(db),
+		uniqueRepo:    service.NewUniqueService(db, nil),
+		mediaRepo:     service.NewMediaService(db, nil),
 	}
 }
