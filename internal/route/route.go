5f4501ecb8394a7f339f48de2548cfc3a7c3f2dc
berrylradianh <berrylhamesha@gmail.com>
2024-05-27 18:17:02 +0700

    fix: add route quiz to bracket

--- internal/route/route.go
+++ internal/route/route.go
@@ -89,14 +89,18 @@ func NewRouteInit(req request.RouteInit) {
 				materialGroup.GET(":subject_uuid", module.Module.MaterialAll)
 				materialGroup.GET("", module.Module.MaterialFind)
 			}
-			quizGroup := dashboardStudentGroup.Group("/quiz")
-			quizGroup.GET("/quiz_content/:quiz_uuid", module.Quiz.QuizContent)
-			quizGroup.POST("/quiz_submission", module.Quiz.QuizSubmission)
-			quizGroup.GET("/quiz_detail/:quiz_uuid/:test_type_id", module.Quiz.QuizDetail)
-			quizGroup.GET("/quiz_review/:quiz_submission_uuid/:test_type_id", module.Quiz.QuizReview)
-			quizGroup.GET("/quiz_score_all/:sub_modul_uuid", module.Quiz.QuizScoreAll)
-			quizGroup.GET("/quiz_sub_module_all/:module_id/:test_type_id", module.Quiz.QuizSubModuleAll)
-			quizGroup.GET("/quiz_all/:sub_module_uuid/:test_type_id", module.Quiz.QuizAll)
+
+			// Routes for Quiz
+			{
+				quizGroup := dashboardStudentGroup.Group("/quiz")
+				quizGroup.GET("/quiz_content/:quiz_uuid", module.Quiz.QuizContent)
+				quizGroup.POST("/quiz_submission", module.Quiz.QuizSubmission)
+				quizGroup.GET("/quiz_detail/:quiz_uuid/:test_type_id", module.Quiz.QuizDetail)
+				quizGroup.GET("/quiz_review/:quiz_submission_uuid/:test_type_id", module.Quiz.QuizReview)
+				quizGroup.GET("/quiz_score_all/:sub_modul_uuid", module.Quiz.QuizScoreAll)
+				quizGroup.GET("/quiz_sub_module_all/:module_id/:test_type_id", module.Quiz.QuizSubModuleAll)
+				quizGroup.GET("/quiz_all/:sub_module_uuid/:test_type_id", module.Quiz.QuizAll)
+			}
 
 			// Routes for Exercise
 			{
